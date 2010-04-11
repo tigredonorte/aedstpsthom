2e634a27c4541c577f06cb91e6adceaf0c9bb177
tigredonorte3 <tigredonorte3@ce921804-c603-50b7-1f47-0957cd1573f4>
2010-04-11 01:15:19 +0000

    Parte sequencial (sem testar)

---
+++ tp1/dicionario.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   dicionario.h
+ * Author: thompson
+ *
+ * Created on 10 de Abril de 2010, 17:43
+ */
+
+#ifndef _DICIONARIO_H
+#define	_DICIONARIO_H
+
+#include "filap.h"
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Fila Hash;
+
+typedef struct Dicionario_str
+{
+    Hash *hash;
+    int tam;
+}Dicionario;
+
+/* pega o tempo do sistema com a funcao getRusages */
+long getTime();
+
+//Retorna uma valor correspondente a posicao do hash na palavra
+int Char2Indice(char* palavra, int m);
+
+//inicializa o dicionario com o tamanho estimado que ele assumira
+void InicializaDicionario(Dicionario *dic, int tamDic);
+
+//insere uma palavra no dicionario caso ela nao exista,
+//adiciona nova ocorrencia caso exista
+void InserePalavraDicionario(Dicionario *dic, int idDoc, char *palavra);
+
+//pesquisa uma palavra dentro do dicionario
+//retorna o tempo gasto para pesquisar a palavra
+PFila PesquisaPalavraDicionario(Dicionario *dic, char *palavra, long *tempoLatencia);
+
+#endif	/* _DICIONARIO_H */
---
+++ tp1/fila.h
@@ -0,0 +1,48 @@
+/* 
+ * File:   fila.h
+ * Author: thompson
+ *
+ * Created on 10 de Abril de 2010, 19:50
+ */
+
+#ifndef _FILA_H
+#define	_FILA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//apontador para uma celula da fila
+typedef struct CFilaDoc_str *PFilaDoc;
+
+//Item da fila
+typedef struct FItemDoc
+{
+    int idDoc;
+}FItemDoc;
+
+//Celula da fila
+typedef struct CFilaDoc_str
+{
+    FItemDoc item;
+    PFilaDoc prox;
+}CFilaDoc;
+
+//Fila propriamente dita
+typedef struct FilaDoc
+{
+    PFilaDoc frente, tras;
+}FilaDoc;
+
+//Esvazia fila
+void esvaziaFilaDoc(FilaDoc *fila);
+
+//insere novo elemento na fila
+void insereFilaDoc(FItemDoc it, FilaDoc *fila);
+
+//inicializa um novo item
+void inicializaItemDoc(FItemDoc *it, int idDoc);
+
+//verifica se id existe na fila, retorna 1 se existir, 0 se nao existir
+int pesquisaId(FilaDoc *fila, int idDoc);
+#endif	/* _FILA_H */
+
