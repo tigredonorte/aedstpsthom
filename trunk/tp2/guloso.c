8b63d0b29668139d99bd027880f24ec41630dd6d
tigredonorte3 <tigredonorte3@ce921804-c603-50b7-1f47-0957cd1573f4>
2010-04-30 03:41:27 +0000

    TP2 - Arvore Gulosa Pronta

--- trunk/tp2/guloso.c
+++ trunk/tp2/guloso.c
@@ -2,18 +2,33 @@
 
 void insereArvoreGulosa(Arvore *ar, Hash *hash)
 {
+    int i;
     int size = 0;
     itemH *vetor;
     criaVetor(hash, &vetor, &size);
+
     quickSort(vetor, size);
 
-    int i;
-    for(i = 0; i < size; i++)
+    for(i = size-1 ; i >= 0; i--)
     {
         Registro x;
         InicializaRegistro(&x, vetor[i].chave);
         InsereArvore(ar, x);
     }
+
+    #ifndef _DEBUG_
+    #define _DEBUG_
+        int lol;
+        char **vS = malloc(sizeof(char*) * size);
+        int *vN = malloc(size * sizeof(int));
+        criaVetorProfundidadeArvore(ar, &vS, &vN);
+        for(lol = 0; lol < size; lol++)
+        {
+            printf("%d ",vN[lol]);
+        }
+        PrintArvore(ar);
+        printf("\n\n ");
+    #endif
 }
 
 void quickSort( itemH* vetor, int N)
@@ -28,12 +43,13 @@ void quickSortOrdena(itemH* vetor, int esq, int dir)
     itemH x;
     i = esq;
     j = dir;
-    x = vetor[(i + j)/2];
+    int m = (i + j)/2;
+    x = vetor[m];
     do
     {
-        while(comparaPopularidadeChave(&vetor[i], &x) != -1){i++;}
+        while(comparaPopularidadeChave(&vetor[i], &x) < 0){i++;}
 
-        while(comparaPopularidadeChave(&vetor[j], &x) != 1){j--;}
+        while(comparaPopularidadeChave(&vetor[j], &x) > 0){j--;}
 
         if(i <= j)
         {
