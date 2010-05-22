fbd269afd3505c9635e7d8610ec6b342e2378e1a
tigredonorte3 <tigredonorte3@ce921804-c603-50b7-1f47-0957cd1573f4>
2010-05-22 10:48:56 +0000

    tp6 copilaçao basica do projeto

---
+++ tp6/TP6/src/main.c
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.c
+ * Author: thompson
+ *
+ * Created on 22 de Maio de 2010, 07:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
