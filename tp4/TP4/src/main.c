13eeeb9bbbc90ca4d3d0905c19cea221976e8e3c
tigredonorte3 <tigredonorte3@ce921804-c603-50b7-1f47-0957cd1573f4>
2010-05-22 10:48:01 +0000

    tp4 copilaçao basica do projeto

---
+++ tp4/TP4/src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: thompson
+ *
+ * Created on 22 de Maio de 2010, 07:45
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    return (EXIT_SUCCESS);
+}
+
