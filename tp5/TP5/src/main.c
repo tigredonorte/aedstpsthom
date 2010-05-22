57e8022c36bc7a00c770d9def7fdd8365c2a915b
tigredonorte3 <tigredonorte3@ce921804-c603-50b7-1f47-0957cd1573f4>
2010-05-22 10:48:32 +0000

    tp5 copilaçao basica do projeto

---
+++ tp5/TP5/src/main.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.c
+ * Author: thompson
+ *
+ * Created on 22 de Maio de 2010, 07:46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    return (EXIT_SUCCESS);
+}
+
